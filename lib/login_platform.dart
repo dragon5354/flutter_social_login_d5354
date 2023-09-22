669ea2a7777932f3c10c85e5094e48e4e189a01d
dragon5354 <dragon5354@naver.com>
2023-09-22 17:32:09 +0900

    카카오 로그인

---
+++ lib/login_platform.dart
@@ -0,0 +1,7 @@
+enum LoginPlatform {
+  google,
+  kakao,
+  naver,
+  // none상태가 로그인하지 않은 기본 상태
+  none,
+}
---
+++ lib/sample_screen.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:flutter_social_login_d5354/login_platform.dart';
+
+
+class SampleScreen extends StatefulWidget {
+  const SampleScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SampleScreen> createState() => _SampleScreenState();
+}
+
+class _SampleScreenState extends State<SampleScreen> {
+  LoginPlatform _loginPlatform = LoginPlatform.none;
+
+
+  // 카카오 로그인용 코드
+  void signInWithKakao() async {
+    try {
+      bool isInstalled = await isKakaoTalkInstalled();
+
+      OAuthToken token = isInstalled
+          ? await UserApi.instance.loginWithKakaoTalk()
+          : await UserApi.instance.loginWithKakaoAccount();
+
+      final url = Uri.https('kapi.kakao.com', '/v2/user/me');
+
+      final response = await http.get(
+        url,
+        headers: {
+          HttpHeaders.authorizationHeader: 'Bearer ${token.accessToken}'
+        },
+      );
+
+      final profileInfo = json.decode(response.body);
+      print(profileInfo.toString());
+
+      setState(() {
+        _loginPlatform = LoginPlatform.kakao;
+      });
+
+    } catch (error) {
+      print('카카오톡으로 로그인 실패 $error');
+    }
+  }
+
+  // 로그아웃 기능
+  void signOut() async {
+    switch (_loginPlatform) {
+      case LoginPlatform.google:
+        break;
+      case LoginPlatform.kakao:
+        await UserApi.instance.logout();
+        break;
+      case LoginPlatform.naver:
+        break;
+      case LoginPlatform.none:
+        break;
+    }
+
+    setState(() {
+      _loginPlatform = LoginPlatform.none; // 상태를 none으로 돌려줌
+    });
+  }
+
+  // 메인 화면
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: _loginPlatform != LoginPlatform.none
+              ? _logoutButton()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _loginButton(
+                      'kakao_logo',
+                      signInWithKakao,
+                    )
+                  ],
+                )),
+    );
+  }
+
+  // 로그인 버튼
+  Widget _loginButton(String path, VoidCallback onTap) {
+    return Container(
+      child: Ink.image(
+        image: AssetImage('asset/image/$path.png'),
+        width: 300,
+        height: 60,
+        fit: BoxFit.cover,
+        child: InkWell(
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+
+  // 로그아웃 버튼
+  Widget _logoutButton() {
+    return ElevatedButton(
+      onPressed: signOut,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          const Color(0xff0165E1),
+        ),
+      ),
+      child: const Text('로그아웃'),
+    );
+  }
+}
